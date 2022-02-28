dcda392b5f93b3d1ecc60623de1004d25b4148d3
Abdulwahid Barguzar <abdulwahid.barguzar@netguru.com>
2022-03-01 00:36:49 +0100

    restructure Player class with event dispatcher.

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,54 +1,75 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import StringProperty, ObjectProperty, BooleanProperty
+from kivy.properties import StringProperty, ObjectProperty, BooleanProperty, NumericProperty
+from kivy.event import EventDispatcher
 
-result = {}
 
-def current_player():
-    if player1.active:
-        player1.active = False
-        player2.active = True
-        return player1
-    elif player2.active:
-        player1.active = True
-        player2.active = False
-        return player2
+class Player(EventDispatcher):
+    active = BooleanProperty(False)
+    display_score = StringProperty("0")
+    name = StringProperty("")
+    score = NumericProperty(0)
 
-class Player:
-    def __init__(self, name, dice, active=False) -> None:
-        self.name = name
+    def __init__(self, name, dice, active=False, **kwargs) -> None:
+        super(Player, self).__init__(**kwargs)
         self.dice = dice
-        self.score = ObjectProperty(None)
         self.active = active
-        self.display_name = StringProperty()
-        '''
-        '{}{} ({})'.format(
-            '*' if not active else '', name, dice)
-        '''
+        self.name = name
 
 class TicTacToe(Screen):
     
-    def on_enter(self, *args):
-        self._update_display_name()
-        return super().on_enter(*args)
+    player1 = Player(name='Player 1', dice='X', active=True)
+    player2 = Player(name='Player 2', dice='O')
+    
+    matrix = {'00': None, '01': None, '02': None, '10': None, '11': None, '12': None, '20': None, '21': None, '22': None}
+    
+    def on_pre_enter(self, *args):
+        for tile_id in self.matrix.keys():
+            setattr(self, 'tile_%s' % tile_id, ObjectProperty(None))
+        return super().on_pre_enter(*args)
     
     def on_leave(self, *args):
+        self._reset_tiles()
         return super().on_leave(*args)
     
+    def _current_player(self):
+        player = self.player1 if self.player1.active else self.player2
+        self.player1.active = not self.player1.active
+        self.player2.active = not self.player2.active
+        return player
+        
+    def _reset_tiles(self):
+        for tile in self.matrix.keys():
+            self.ids['root.tile_%s' % tile].text = ''
+
+    def _check_rows(self):
+        x_points = 0
+        o_points = 0
+        for row in range(3):
+            for col in range(3):
+                dice = self.matrix['{row}{col}'.format(row=row, col=col)]
+                print(dice)
+                
+    def _check_cols(self):
+        pass
+                
+    def _check_diagonal(self):
+        pass
+    
+    def _check_move(self):
+        pass
+    
+    def _game_over():
+        pass
+    
+    def restart(self):
+        self._reset_tiles()
+
     def reset_scores(self):
-        self.ids['player1.score'].text = '0'
-        self.ids['player2.score'].text = '0'
-    
-    def _update_display_name(self):
-        self.ids['player1.display_name'].text = '{}{} ({})'.format(
-            '*' if player1.active else '', player1.name, player1.dice)
-        self.ids['player2.display_name'].text = '{}{} ({})'.format(
-            '*' if player2.active else '', player2.name, player2.dice)
-    
+        self.player1.score += 1
+        self.player2.score += 1
+        
     def make_move(self, tile):
         if not tile.text:
-            player = current_player()
-            tile.text = player.dice
-            self._update_display_name()
-
-player1 = Player(name='Player 1', dice='X', active=True)
-player2 = Player(name='Player 2', dice='O')
+            player = self._current_player()
+            tile.text = self.matrix[tile.name] = player.dice
+            self._check_move()
